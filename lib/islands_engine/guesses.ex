4927ea4b11779670739c7a6f2a46b8abe869cf17
Barbara Jaber <bpolianajaber@gmail.com>
2022-01-22 15:44:09 -0300

    feat: add method that add hited and missed coordinates to Guesses

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,10 +1,17 @@
 defmodule IslandsEngine.Guesses do
   alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [:hits, :misses]
 
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate),
+    do: update_in(guesses.hits, &MapSet.put(&1, coordinate))
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate),
+    do: update_in(guesses.misses, &MapSet.put(&1, coordinate))
 
   # {:ok, coordinate1} = Coordinate.new(1,1)
   # {:ok, coordinate2} = Coordinate.new(2,2)
