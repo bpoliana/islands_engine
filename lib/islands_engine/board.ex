229cf28cf80e8f3f97c6abc45c884fa2d8bc00d5
Barbara Jaber <bpolianajaber@gmail.com>
2022-01-23 21:42:22 -0300

    fix: fix Coordinate alias at Board module

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Board do
-  alias IslandsEngine.Island
+  alias IslandsEngine.{Island, Coordinate}
 
   def new(), do: %{}
 
@@ -13,7 +13,7 @@ defmodule IslandsEngine.Board do
   # gets the list of valid island types from the Island.types/0
   def all_islands_positioned?(board), do: Enum.all?(Island.types(), &Map.has_key?(board, &1))
 
-  def guess(board, %Cooridnate{} = coordinate) do
+  def guess(board, %Coordinate{} = coordinate) do
     board
     |> check_all_islands(coordinate)
     |> guess_response(board)
