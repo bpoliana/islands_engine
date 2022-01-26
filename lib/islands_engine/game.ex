89ce989c7a2633b577906593d21f63cbaf018632
Barbara Jaber <bpolianajaber@gmail.com>
2022-01-26 18:29:00 -0300

    wip: add demo_cast methods to Game - GenServer

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -5,6 +5,10 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, :demo_call)
   end
 
+  def demo_cast(pid, new_value) do
+    GenServer.cast(pid, {:demo_cast, new_value})
+  end
+
   def handle_info(:first, state) do
     IO.puts("This message has been handled by handle_info/2, matching on :first")
     {:noreply, state}
@@ -12,5 +16,9 @@ defmodule IslandsEngine.Game do
 
   def handle_call(:demo_call, _from, state) do
     {:reply, state, state}
+  end
+
+  def handle_cast({:demo_cast, new_value}, state) do
+    {:noreply, Map.put(state, :test, new_value)}
   end
 end
